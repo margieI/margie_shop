6e8422000858307e2d80547ae340cfd948da8576
Margarita Itaj <itaimargi@hotmail.com>
2015-08-05 23:22:55 +0300

    scaffold product

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150804195425) do
+ActiveRecord::Schema.define(version: 20150805192931) do
 
   create_table "products", force: true do |t|
     t.string   "name"
