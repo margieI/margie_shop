09c04829a78dc03e6e39d0380247408302b5ca57
margieI <itaimargi@hotmail.com>
2015-08-28 12:20:08 +0300

    Image Carousel exe 35

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,3 +10,9 @@ class StaticPagesController < ApplicationController
   end
 
 end
+
+class StaticPageController < ApplicationController
+   def landing_page
+    @products = Product.limit(3)
+   end
+end
