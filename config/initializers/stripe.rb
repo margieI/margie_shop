7936f5945a8516068385c05d3709daf749bcbc49
margieI <itaimargi@hotmail.com>
2015-10-11 12:43:41 +0300

    'exe52 Payment Forms'

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,23 @@
-Rails.configuration.stripe = {
-  :publishable_key => ENV['PUBLISHABLE_KEY'],
-  :secret_key      => ENV['SECRET_KEY']
-}
+#Rails.configuration.stripe = {
+#  :publishable_key => ENV['PUBLISHABLE_KEY'],
+#  :secret_key      => ENV['SECRET_KEY']
+#}
+
+#Stripe.api_key = Rails.configuration.stripe[:secret_key
+
+
+
+
+	if Rails.env.production?
+  Rails.configuration.stripe = {
+    :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+    :secret_key => ENV['STRIPE_SECRET_KEY']
+  }
+else
+  Rails.configuration.stripe = {
+    :publishable_key => 'pk_test_YHTJW4W3USDngOyaPQ51VWnh',
+    :secret_key => 'sk_test_fNwgotYuoc8npvLLs8KoPJXD'
+  }
+end
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
